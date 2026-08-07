a2a3ab130abf557bc3c6c4ec3327bb535805241b
nikhil <nksaini1322@email.com>
2026-08-07 19:20:01 +0530

    new chdnages

--- client/src/pages/admin/LicenceVerification.jsx
+++ client/src/pages/admin/LicenceVerification.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LicenceVerification() {
+  const [licences, setLicences] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedLicence, setSelectedLicence] = useState(false);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [search, setSearch] = useState("");
+  const [status, setStatus] = useState("All");
+
   return <div>working</div>;
 }
 
